8a2df879e844531ae2bd6aa446eb07d02482b997
Devin Gray <devingray@gmail.com>
2016-07-12 16:40:53 -0600

    start a lib prime number lib from problem 3 and 7

---
+++ projecteuler/lib/prime.go
@@ -0,0 +1,45 @@
+package lib
+
+import (
+	"container/list"
+	//	"fmt"
+)
+
+func main() {
+}
+
+func isPrime(num int) bool {
+	for i := 2; i <= num/2; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func GetPrimes(number int) *list.List {
+	primes := list.New()
+	for i := number; i > 2; i-- {
+		if isPrime(i) {
+			primes.PushFront(i)
+		} else {
+		}
+	}
+	return primes
+}
+
+func GetNthPrime(n int) int {
+	count := 0
+	number := 2
+	for {
+		//fmt.Println("number=", number)
+		if isPrime(number) {
+			count++
+		}
+		if count >= n {
+			return number
+		}
+		number++
+	}
+	return 0
+}
