5fb293e964dbec0da93697e7e46f882c55245e38
Devin Gray <devingray@gmail.com>
2016-07-17 20:58:31 -0600

    solved #26

--- projecteuler/problem26.go
+++ projecteuler/problem26.go
@@ -23,62 +23,30 @@ package main
 
 import (
 	"fmt"
-	"math/big"
-	"strings"
 )
 
 func main() {
 	fmt.Println("problem 26")
-	//for i := 2; i < 100; i++ {
-	//for i := 24; i < 25; i++ {
-	//for i := 6; i < 7; i++ {
-	for i := 95; i < 96; i++ {
-		v := big.NewRat(int64(1), int64(i))
-		c := recurringCycle(v)
-		s := v.FloatString(100)
-		fmt.Printf("1/%d: %d - %s\n", i, c, s)
-	}
+	sequenceLength := 0
+	for i := 1000; i > 1; i-- {
+		foundRemainders := make([]int, i)
+		value := 1
+		position := 0
 
-}
-
-func recurringCycle(r *big.Rat) int {
-	precision := 200
-	s := strings.Split(r.FloatString(precision), ".")[1]
-	//fmt.Println(s)
-	lastChar := s[len(s)-1 : len(s)]
-	if lastChar == "0" {
-		return 0
-	}
-	//find the pattern
-	a := strings.Split(s, s[0:1])
-	if len(a) > precision {
-		return 1
-	}
-	//try to grown the pattern
-	//fmt.Println("len(s)=", len(s))
-	for t := 0; t < len(s); t++ {
-		for i := t; i < len(s)-t; i++ {
-			//fmt.Println("t=", t)
-			//fmt.Println("i=", i)
-			subStr := s[t : t+i]
-			//fmt.Println("subStr=", subStr)
-			a = strings.Split(s, subStr)
-			total := len(subStr) * len(a)
-			if t > 0 {
-				fmt.Println("t=", t)
-				fmt.Println("len(subStr)=", len(subStr))
-				fmt.Println("len(a)=", len(a))
-				fmt.Println("total=", total)
-				fmt.Println("precision=", precision)
-				fmt.Println("subStr=", subStr)
-				fmt.Println("len substring = ", len(subStr))
-			}
-			if len(a) > len(subStr) && total >= precision-t && (len(subStr) < precision/2) {
-				fmt.Println(subStr)
-				return i
+		for {
+			if foundRemainders[value] == 0 && value != 0 {
+				foundRemainders[value] = position
+				value = value * 10
+				value = value % i
+				position++
+			} else {
+				break
 			}
 		}
-	}
-	return -1
 
+		if position-foundRemainders[value] > sequenceLength {
+			sequenceLength = position - foundRemainders[value]
+		}
+	}
+	fmt.Println("sequenceLength=", sequenceLength)
 }
