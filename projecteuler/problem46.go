4e0792e8dd7e107ef82606c19bf177491a6acb91
Devin Gray <devingray@gmail.com>
2016-07-31 16:07:47 -0600

    add name

--- projecteuler/problem46.go
+++ projecteuler/problem46.go
@@ -1,6 +1,6 @@
 /*
 https://projecteuler.net/problem=46
-<NAME>
+Goldbach's other conjecture
 
 It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
 
