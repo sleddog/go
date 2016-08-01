e010dc0e3d4ceddbfd0acef744602e58ef7d1579
Devin Gray <devingray@gmail.com>
2016-07-31 21:28:31 -0600

    wip

--- projecteuler/problem47.go
+++ projecteuler/problem47.go
@@ -9,7 +9,7 @@ The first two consecutive numbers to have two distinct prime factors are:
 
 The first three consecutive numbers to have three distinct prime factors are:
 
-644 = 2 * 7 * 23
+644 = 2^2 * 7 * 23
 645 = 3 * 5 * 43
 646 = 2 * 17 * 19
 
@@ -27,17 +27,22 @@ var p []int
 
 func main() {
 	fmt.Println("problem 47")
-	size := 16
+	size := 651
 	p = lib.GetPrimesSieve(size)
-	fmt.Println(p)
-	distinctTwoPrimeFactors(size)
+
+	//s := distinctTwoPrimeFactors(size)
+	//fmt.Println(s)
+
+	s := distinctThreePrimeFactors(size)
+	fmt.Println(s)
 }
 
-func distinctTwoPrimeFactors(size int) {
+func distinctTwoPrimeFactors(size int) string {
 	d := 2
 	fmt.Printf("looking for first %v consecutive numbers to have %v distinct prime factors\n", d, d)
 
 	lastFound := 0
+	s := ""
 
 	for i := 1; i < size; i++ {
 		for _, v1 := range p {
@@ -46,14 +51,58 @@ func distinctTwoPrimeFactors(size int) {
 					continue
 				}
 				if v1*v2 == i {
-					fmt.Printf("%v * %v = %v\n", v1, v2, i)
 					if lastFound+1 == i {
-						fmt.Println("consecutive")
-						return
+						s += fmt.Sprintf("%v * %v = %v\n", v1, v2, i)
+						return s
 					}
+					s = fmt.Sprintf("%v * %v = %v\n", v1, v2, i)
 					lastFound = i
 				}
 			}
 		}
 	}
+	return "?"
+}
+
+func distinctThreePrimeFactors(size int) string {
+	d := 3
+	fmt.Printf("looking for first %v consecutive numbers to have %v distinct prime factors\n", d, d)
+
+	count := 0
+	prev := -1
+	s := ""
+	product := 0
+
+	for i := 1; i < size; i++ {
+		for _, v1 := range p {
+			for _, v2 := range p {
+				if v1 >= v2 {
+					continue
+				}
+				for _, v3 := range p {
+					if v2 >= v3 {
+						continue
+					}
+					//to capture 644... need to square each term as well???
+					product = v1 * v2 * v3
+					if product == i {
+						fmt.Printf("i==%v   -    %v * %v * %v = %v\n", i, v1, v2, v3, product)
+						if prev == i-1 {
+							count++
+							s += fmt.Sprintf("%v * %v * %v = %v\n", v1, v2, v3, i)
+						} else {
+							count = 1
+							s = fmt.Sprintf("%v * %v * %v = %v\n", v1, v2, v3, i)
+						}
+						prev = i
+
+						if count == d {
+							return s
+						}
+					}
+				}
+			}
+		}
+	}
+	return "?"
 }
